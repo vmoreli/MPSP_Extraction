f081ff47d855327d873f0b462752300923bbaee4
Gustavo Moraes <130313875+Gucmm@users.noreply.github.com>
2026-06-26 18:51:32 -0300

    Delete extraction_pipeline directory

--- extraction_pipeline/schemas/eval_schemas.py
+++
@@ -1,4 +0,0 @@
-from pydantic import BaseModel, Field
-
-class Equal(BaseModel):
-    are_the_strings_equal: bool = Field(False, description="Indicates wether the strings being compared are equal (true) or not (false).")
